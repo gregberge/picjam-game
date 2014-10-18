88b69da51471e9155a8e66f7be36f92a8831befa
eloone <elodie.rafali@gmail.com>
2014-10-18 20:52:49 +0200

    design attempt

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   game: {
     nbUsers: 3,
-    timeBeforeStart: 1000,
-    timeBetweenQuestions: 1000,
+    timeBeforeStart: 5000,
+    timeBetweenQuestions: 5000,
     questionTime: 3000,
     points: [3, 2, 1],
     nbQuestions: 5
